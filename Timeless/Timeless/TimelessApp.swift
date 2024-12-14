5c3f00bea5c95e5227f96f3550e2715ba8ec12f2
Michele Coppola <michele.coppola17@outlook.it>
2024-12-15 00:14:13 +0100

    Initial project setup.

---
+++ Timeless/Timeless/TimelessApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimelessApp.swift
+//  Timeless
+//
+//  Created by Michele Coppola on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TimelessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
