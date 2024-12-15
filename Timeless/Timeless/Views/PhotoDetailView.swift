57a98849b6e59d10ec210f4127f9e58164b05bca
Michele Coppola <michele.coppola17@outlook.it>
2024-12-15 13:48:07 +0100

    Completed PhotoDetailView.

--- Timeless/Timeless/Views/PhotoDetailView.swift
+++ Timeless/Timeless/Views/PhotoDetailView.swift
@@ -11,7 +11,36 @@ struct PhotoDetailView: View {
     let photo: Photo
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                AsyncImage(url: URL(string: photo.edmPreview[0]), scale: 3) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 350)
+                
+                HStack {
+                    Text(photo.dcCreator?.first ?? "Unknown Author")
+                        .font(.headline)
+                        .fontWeight(.bold)
+                        .padding(10)
+                        .foregroundStyle(.white)
+                        .background(.gray)
+                        .clipShape(.capsule)
+                    
+                    Text(photo.edmTimespanLabelLangAware?.zxx?.first ?? "Unknown Date")
+                }
+                
+                Text((photo.dcDescriptionLangAware?.en?.first ?? photo.dcDescription?.first) ?? "No Description")
+                    .padding()
+                
+                Spacer()
+            }
+            .navigationTitle((photo.dcTitleLangAware.en?.first ?? photo.title?.first) ?? "Untitled")
+        }
     }
 }
 
